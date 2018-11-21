e96d2967f24e30504411603aa63946ec8b52b3cd
Marcus Legendre <marcus.legendre@gmail.com>
2018-11-21 04:08:41 +0100

    Defined abstract abstract classes for generic problem formalization

---
+++ problems/abstract_problem.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod, abstractstaticmethod
+from typing import List
+
+
+class AbstractState(ABC):
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class AbstractAction(ABC):
+
+    @abstractstaticmethod
+    def execute(state: AbstractState) -> AbstractState:
+        pass
+
+
+class AbstractProblem(ABC):
+    """Formalization of an abstract problem"""
+
+    @abstractstaticmethod
+    def create_initial_state() -> AbstractState:
+        pass
+
+    @abstractstaticmethod
+    def get_actions() -> List[AbstractAction]:
+        pass
+
+    @abstractstaticmethod
+    def is_goal_state(state) -> bool:
+        pass
