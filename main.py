b27b4500e662fdf966400c464f85937f38e83657
Marcus Legendre <marcus.legendre@gmail.com>
2018-11-21 08:08:22 +0100

    Solve both the simplified and the standard Water Jug Problem in main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 #!/bin/python3
 
 from problems.water_jug_problem import WaterJugProblem
+from problems.standard_water_jug_problem import StandardWaterJugProblem
 from strategies.breadth_first_search import BreadthFirstSearch
 
-problem = WaterJugProblem
-strategy = BreadthFirstSearch(problem)
+problems = [
+    WaterJugProblem,
+    StandardWaterJugProblem,
+]
 
-strategy.find_solution()
-strategy.print_solution()
+for problem in problems:
+    problem = problem
+    print("Trying to find a solution for", problem.__name__)
+    strategy = BreadthFirstSearch(problem)
+    strategy.find_solution()
+    strategy.print_solution()
+    print("\n")
