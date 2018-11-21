0a062e9704090af304f797c0b8af231f0fbeeaaf
Marcus Legendre <marcus.legendre@gmail.com>
2018-11-21 04:09:49 +0100

    Refactored Water Jug Problem to make use of the newly defined abstract classes

---
+++ problems/water_jug_problem.py
@@ -0,0 +1,98 @@
+from functools import partial
+from typing import List
+
+from abstract_problem import AbstractAction, AbstractProblem, AbstractState
+
+
+class WaterJugState(AbstractState):
+
+    def __init__(self, gallons_big_jug: int, gallons_small_jug: int):
+        self.big_jug = gallons_big_jug
+        self.small_jug = gallons_small_jug
+
+    def __str__(self) -> str:
+        return "(" + str(self.big_jug) + ", " + str(self.small_jug) + ")"
+
+
+class WaterJugProblem(AbstractProblem):
+
+    @staticmethod
+    def create_initial_state() -> WaterJugState:
+        return WaterJugState(0, 0)
+
+    @staticmethod
+    def get_actions() -> List[AbstractAction]:
+        return [
+            BigToSmall.execute,
+            SmallToBig.execute,
+            EmptyBig,
+            EmptySmall,
+            FillBig,
+            FillSmall,
+        ]
+
+    @staticmethod
+    def goal_reached(state: WaterJugState) -> bool:
+        return state.big_jug == 2
+
+
+class BigToSmall(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        big = max(0, state.big_jug + state.small_jug - 3)
+        small = min(3, state.big_jug + state.small_jug)
+        return WaterJugState(big, small)
+
+
+class SmallToBig(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        big = min(4, state.big_jug + state.small_jug)
+        small = max(0, state.big_jug + state.small_jug - 4)
+        return WaterJugState(big, small)
+
+
+class EmptyBig(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        return WaterJugState(0, state.small_jug)
+
+
+class EmptySmall(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        return WaterJugState(state.big_jug, 0)
+
+
+class FillBig(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        return WaterJugState(4, state.small_jug)
+
+
+class FillSmall(AbstractAction):
+
+    @staticmethod
+    def execute(state: WaterJugState) -> WaterJugState:
+        return WaterJugState(state.big_jug, 3)
+
+
+if __name__ == "__main__":
+    state = WaterJugProblem.create_initial_state()
+    path = [
+        FillBig.execute,
+        BigToSmall.execute,
+        EmptySmall.execute,
+        BigToSmall.execute,
+        FillBig.execute,
+        BigToSmall.execute,
+    ]
+    for action in path:
+        state = partial(action, state)()
+        print(state)
+    print(WaterJugProblem.goal_reached(state))
