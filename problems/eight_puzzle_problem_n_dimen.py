98f882938e0b01d3d4feeaec36bb6227ebf985e6
saidar <ramazanov13@gmail.com>
2019-01-29 21:25:41 +0100

    #fixed all problems, works on small problems

--- problems/eight_puzzle_problem_n_dimen.py
+++ problems/eight_puzzle_problem_n_dimen.py
@@ -32,12 +32,13 @@ class EightPuzzleProblemStateNDim(AbstractProblemState):
     # 0 1 2
     # 3 4 5
     # 6 7 8
-    # goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+    state = [4, 1, 2, 3, 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
-    def __init__(self, heuristic=0, n=3):
+    def __init__(self, state, heuristic=0, n=3):
         self.n = n  # dimension
-        self.goal_state = np.array(range(0, n ** 3))  # depending on dimension we create new goal state
-        self.state = create_initial_state_state(n)  # state # shuffles the goal_state and checks if it is decidable
+        # self.goal_state = goal_state  # depending on dimension we create new goal state
+        self.state = state # create_initial_state_state(n)  # state # shuffles the goal_state and checks if it is decidable
         self.use_heuristic_num = heuristic
 
     def __str__(self):
@@ -58,21 +59,22 @@ class EightPuzzleProblemStateNDim(AbstractProblemState):
 
     @staticmethod
     def create_initial_state(heuristic=0):
-        return EightPuzzleProblemStateNDim(heuristic, 3)
+        return EightPuzzleProblemStateNDim(create_initial_state_state(3), heuristic, 3)
 
     def get_actions(self):
         actions = []
-        if self.tile_position(0) % (self.n ** 2) > 2:
+        title_pos = self.tile_position(0)[0] # should be converted
+        if title_pos % (self.n ** 2) > 2:
             actions.append(self.top_down)
-        if self.tile_position(0) % (self.n ** 2) < 6:
+        if title_pos % (self.n ** 2) < 6:
             actions.append(self.bottom_up)
-        if self.tile_position(0) % (self.n ** 2) not in [0, 3, 6]:
+        if title_pos % (self.n ** 2) not in [0, 3, 6]:
             actions.append(self.left_right)
-        if self.tile_position(0) % (self.n ** 2) not in [2, 5, 8]:
+        if title_pos % (self.n ** 2) not in [2, 5, 8]:
             actions.append(self.right_left)
-        if self.tile_position(0) > (self.n ** 2 - 1):
+        if title_pos > (self.n ** 2 - 1):
             actions.append(self.foward_backwards)
-        if self.tile_position(0) < (2 * self.n ** 2):
+        if title_pos < (2 * self.n ** 2):
             actions.append(self.backwards_forwards)
         return actions
 
@@ -113,50 +115,57 @@ class EightPuzzleProblemStateNDim(AbstractProblemState):
         return int(abs(a_x - b_x) + abs(a_y - b_y))
 
     def is_goal_state(self):
-        return self.state.all() == self.goal_state.all()
+
+        first = self.state
+        second = self.goal_state
+
+        if np.array_equal(first, second):
+            return True;
+
+        return False # self.state.all() == self.goal_state.all() if True else False
 
     def tile_position(self, n: int) -> int:
         """Returns the index of the field containing n"""
-        return self.state.index(n)
+        return self.index(n) # there should be another function
 
     def top_down(self) -> 'EightPuzzleProblemStateNDim':
         """Move tile above the 0 down"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 - self.n] = new_state[p0 - self.n], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
 
     def bottom_up(self) -> 'EightPuzzleProblemStateNDim':
         """Move tile below the 0 up"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 + self.n] = new_state[p0 + self.n], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
 
     def left_right(self) -> 'EightPuzzleProblemStateNDim':
         """Move tile left of 0 right"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 - 1] = new_state[p0 - 1], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
 
     def right_left(self) -> 'EightPuzzleProblemState':
         """Move tile right of 0 left"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 + 1] = new_state[p0 + 1], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
 
     def foward_backwards(self) -> 'EightPuzzleProblemStateNDim':
         """Move tile forward of 0 backwards"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 - self.n ** 2] = new_state[p0 - self.n ** 2], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
 
     def backwards_forwards(self) -> 'EightPuzzleProblemStateNDim':
         """Move tile backwards of 0 forward"""
-        p0 = self.tile_position(0)
+        p0 = self.tile_position(0)[0]
         new_state = self.state.copy()
         new_state[p0], new_state[p0 + self.n ** 2] = new_state[p0 + self.n ** 2], new_state[p0]
         return EightPuzzleProblemStateNDim(new_state, self.use_heuristic_num)
@@ -165,3 +174,8 @@ class EightPuzzleProblemStateNDim(AbstractProblemState):
     def get_goal_state(n=3) -> []:
         dim = np.power(3, 3)
         return np.array(range(0, dim))
+
+    def index(self, item):
+        for idx, val in np.ndenumerate(self.state):
+            if val == item:
+                return idx
